e1926d9b94cc1b353298d73539c0d16ab7f8e0b3
BHARGAVSAI558 <bhargavsai954@gmail.com>
2025-08-06 09:39:12 +0530

    jenkins react app v1.0

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base:'jenkenserp' //deployement folder but not repo name
+})
